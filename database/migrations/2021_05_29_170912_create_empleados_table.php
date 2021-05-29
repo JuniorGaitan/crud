18e889642e609d1490e8a1682352c043670a8514
unknown <JuniorGaitan>
2021-05-29 12:23:12 -0600

    first commit

---
+++ database/migrations/2021_05_29_170912_create_empleados_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('cedula');
+            $table->string('telefono');
+            $table->integer('cargo_id')->unsigned();            
+            $table->foreign('cargo_id')->references('id')->on('cargos');
+            $table->string('email')->unique();
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
